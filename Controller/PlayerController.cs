a22c5ef221a368600efbdd79f39a9afc0c5b643f
mmar58 <rahmanapu118@gmail.com>
2025-11-18 19:19:30 +0600

    Added ismoving variable and moving animation

--- Controller/PlayerController.cs
+++ Controller/PlayerController.cs
@@ -11,18 +11,39 @@ namespace MMARGame.Controller
     {
         public float moveSpeed = 3;
         public Vector3 dir;
-        float hzInput, vInput;
-        CharacterController controller;
+        
         public Transform player;
         [SerializeField] float groundYOffset;
         [SerializeField] LayerMask groundMask;
         Vector3 spherePos;
         [SerializeField] float gravity = -9.81f;
         Vector3 velocity;
+        [Header("Config")]
+        public bool lookWithMovement;
+        [Header("Animation Variables")]
+        public string walkAnimParam = "isWalking";
+
+        CharacterController controller;
+        Animator animator;
+        #region Input Variables
+        float hzInput, vInput;
         private PlayerInput playerInput;
         private InputAction moveAction;
+
+        bool _isMoving = false;
+        public bool isMoving
+        {
+            get { return _isMoving; }
+            set { _isMoving = value;
+                // Setting movement animation
+                animator?.SetBool(walkAnimParam, value);
+            }
+        }
+        #endregion
+
+        #region MonoBehaviour Lifecycle
         // Start is called before the first frame update
-        void Start()
+        public virtual void Start()
         {
             controller = GetComponent<CharacterController>();
             playerInput = GetComponent<PlayerInput>();
@@ -30,14 +51,25 @@ namespace MMARGame.Controller
             {
                 moveAction = playerInput.actions["Move"];
             }
+            #region Grabbbing Animator
+            if (player != null)
+            {
+                animator = player.GetComponent<Animator>();
+            }
+            if(animator == null)
+            {
+                animator = GetComponent<Animator>();
+            }
+            #endregion
         }
 
         // Update is called once per frame
-        void Update()
+        public virtual void Update()
         {
             GetDirectionAndMove();
             Gravity();
         }
+        #endregion
         public void AimAt(Vector3 position)
         {
             position.y=player.position.y;
@@ -50,11 +82,32 @@ namespace MMARGame.Controller
                 Vector2 input = moveAction.ReadValue<Vector2>();
                 hzInput = input.x;
                 vInput = input.y;
+                if(hzInput!=0 || vInput!=0)
+                {
+                    if(lookWithMovement)
+                    {
+                        Vector3 lookDir = transform.forward * vInput + transform.right * hzInput;
+                        if(player == null)
+                        {
+                            transform.rotation = Quaternion.LookRotation(lookDir);
+                        }
+                        else
+                        {
+                            player.rotation = Quaternion.LookRotation(lookDir);
+                        }
+                    }
+                    isMoving = true;
+                }
+                else
+                {
+                    isMoving = false;
+                }
             }
             else
             {
                 hzInput = 0;
                 vInput = 0;
+                isMoving = false;
             }
             dir = transform.forward * vInput + transform.right * hzInput;
             controller.Move(dir.normalized * moveSpeed * Time.deltaTime);
